9605039529a95b677677d2e031f99416082d949a
tauhidurrana <tauhidurrahman.rana@gmail.com>
2022-05-07 20:41:36 +0600

    navbar & banner added

---
+++ src/Pages/Header/Header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+const Header = () => {
+    return (
+        <div>
+           <>
+            <Navbar sticky="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand as={Link} to="/">Electronics Warehouse Management</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link as={Link} to="/home">Home</Nav.Link>
+                            <Nav.Link as={Link} to="/items">Inventory Items</Nav.Link>
+                            <Nav.Link as={Link} to="/myitems">My Items</Nav.Link>
+                            
+                        </Nav>
+                        <Nav>
+                            <Nav.Link as={Link} to="/about">About</Nav.Link>
+                            {/* {
+                                user?
+                                <Button style={{ textDecoration: 'none' }} variant="link" onClick={handleSignOut}>Sign out</Button>
+                                :
+                            <Nav.Link as={Link} to="/login">
+                                Login</Nav.Link>} */}
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+        </div>
+    );
+};
+
+export default Header;
